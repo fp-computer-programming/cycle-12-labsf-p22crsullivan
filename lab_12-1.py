12ed24e97a6a86edb346f28ab3d21be87c9d66ed
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-02-15 10:02:14 -0500

    Submit Lab 12

---
+++ lab_12-1.py
@@ -0,0 +1,6 @@
+# Author: CRS 02/15/22
+ofile = open("my_file.txt", "w")
+ofile.write("CRS 02/15/22\n")
+ofile.write("Hello World!\n")
+ofile.write("I play video games for fun.")
+ofile.close()
---
+++ lab_12-2.py
@@ -0,0 +1,5 @@
+# Author: CRS 02/15/22
+ofile = open("my_file.txt", "a")
+ofile.write("\nI play games with my friends during isolation.\n")
+ofile.write("Once isolation is over I will likely travel more.")
+ofile.close()
