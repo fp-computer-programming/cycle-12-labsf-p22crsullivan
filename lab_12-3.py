f4d8a156503a0251e2dc12d400ead555a3c2a105
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-02-16 09:51:17 -0500

    Submit Lab 12-3 and 4

---
+++ lab_12-3.py
@@ -0,0 +1,5 @@
+# Author: CRS 02/16/22
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+weekend = ("Saturday", "Sunday")
+days_of_the_week = weekdays + weekend
+print(days_of_the_week)
